8d3c317ccac537a054bcfabedde5f4dc66b2c6da
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-04-15 15:05:34 -0300

    ordenando

--- src/setup.py
+++ src/setup.py
@@ -15,7 +15,7 @@ setup(
     #long_description=open('README.txt').read(),
     install_requires=[
        "numpy", #"Django >= 1.1.1",
-       "Pillow",
+       "Pillow", #
        "opencv-python"
     ],
 )
