bb5cd56a65cac41e3d27d5e52f783bf017e523d3
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-07-18 21:14:49 -0300

    ordenando

--- src/VideoImageTools/modulofuncs.py
+++ src/VideoImageTools/modulofuncs.py
@@ -63,3 +63,59 @@ def apply_func_predictor_over_video_outmp4(func,predictor,vin_path,vout_path,sho
     return True;
 
 
+def apply_func_predictor_over_video_to_frames(func, predictor, vin_path, vout_dir, show=False,FORMATO = "frame_{:05d}.png"):
+    # Cria a pasta de saída se não existir
+    if not os.path.exists(vout_dir):
+        os.makedirs(vout_dir)
+    
+    # Captura de vídeo
+    cap = cv2.VideoCapture(vin_path)
+    
+    # Verifica a versão do OpenCV
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+    if int(major_ver) < 3:
+        fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+        total_frames = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+    else:
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+    # Salva o FPS em um arquivo JSON
+    json_data = {'fps': fps}
+    json_filename = os.path.join(vout_dir, 'video_info.json')
+    with open(json_filename, 'w') as json_file:
+        json.dump(json_data, json_file, indent=4)
+   
+    # Loop pelos frames do vídeo
+    with tqdm(total=total_frames, desc="Processing", bar_format="{l_bar}{bar} [ time left: {remaining} ]") as pbar:
+        frame_count = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            pbar.update(1)
+            
+            if ret:
+                # Aplica a função ao frame
+                processed_frame = func(predictor, frame)
+                
+                # Formata o nome do arquivo
+                frame_filename = os.path.join(vout_dir, FORMATO.format(frame_count))
+                
+                # Salva o frame processado na pasta de saída
+                cv2.imwrite(frame_filename, processed_frame)
+                
+                frame_count += 1
+
+                if show:
+                    cv2.imshow('Frame', processed_frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+            else:
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return True
+
+
