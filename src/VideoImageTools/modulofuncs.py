1783f4322cf0971956cd60c14a274e14188b2829
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-07-18 21:45:14 -0300

    ordenando

--- src/VideoImageTools/modulofuncs.py
+++ src/VideoImageTools/modulofuncs.py
@@ -89,7 +89,7 @@ def apply_func_predictor_over_video_to_frames(func, predictor, vin_path, vout_di
         json.dump(json_data, json_file, indent=4)
    
     # Loop pelos frames do vídeo
-    frame_name=[];
+    image_files=[];
     with tqdm(total=total_frames, desc="Processing", bar_format="{l_bar}{bar} [ time left: {remaining} ]") as pbar:
         frame_count = 0
         while cap.isOpened():
@@ -103,7 +103,7 @@ def apply_func_predictor_over_video_to_frames(func, predictor, vin_path, vout_di
                 # Formata o nome do arquivo
                 frame_filename = os.path.join(vout_dir, FORMATO.format(frame_count))
                 
-                frame_name.append(frame_filename);
+                image_files.append(frame_filename);
                 
                 # Salva o frame processado na pasta de saída
                 cv2.imwrite(frame_filename, processed_frame)
@@ -119,7 +119,7 @@ def apply_func_predictor_over_video_to_frames(func, predictor, vin_path, vout_di
 
     cap.release()
     cv2.destroyAllWindows()
-    return frame_name, fps;
+    return image_files, fps;
 
 # import glob
 # image_files = sorted(glob.glob(os.path.join(images_path, '*.png')))
